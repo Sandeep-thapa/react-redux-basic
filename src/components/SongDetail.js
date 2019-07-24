11e078ccc7a610663add52e9f7d5fa892634eb0c
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-24 13:57:33 +0300

    App completion on react reedux usecasee

---
+++ src/components/SongDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SongList from './SongList';
+
+const SongDetail = ({ song }) => {
+    if (!song) {
+        return <div>Select a song</div>
+    }
+    return (
+        <div className="ui container" >
+            <h3>Details for:</h3>
+            <p>
+                Title:{song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
+        </div>
+    )
+};
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+};
+
+export default connect(mapStateToProps)(SongDetail);
